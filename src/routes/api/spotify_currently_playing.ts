f66447bcadfbdcc44c81663109e6f863c008d076
Mikkel RINGAUD <mikkel@milescode.dev>
2023-03-04 21:06:52 +0100

    feat: add new properties for spotify API

--- src/routes/api/spotify_currently_playing.ts
+++ src/routes/api/spotify_currently_playing.ts
@@ -1,28 +1,43 @@
-import { json } from "solid-start"
+import type { ApiSpotifyCurrentlyPlayingResponse, ApiSpotifyCurrentlyPlayingError } from "~/types/api";
+import type { SpotifyNowPlayingResponse } from "~/types/spotify";
+
+import { json } from "solid-start";
 import { getNowPlaying } from "~/utils/spotify";
 
 export const GET = async () => {
-  const data = await getNowPlaying();
-  console.log(data)
+  const response = await getNowPlaying();
 
-  if (data.status === 204 || data.status > 400) {
-    return json({
+  if (response.status === 204 || response.status > 400) {
+    return json<ApiSpotifyCurrentlyPlayingError>({
       success: false,
-      message: "Nothing is playing!"
+      message: "Spotify is not playing!",
+      debug: { status: response.status }
     });
   }
-  const song = await data.json();
-  const albumImageUrl = song.item.album.images[0].url;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
-  const isPlaying = song.is_playing;
-  const songUrl = song.item.external_urls.spotify;
-  const title = song.item.name;
 
-  return json({
-    albumImageUrl,
-    artist,
-    isPlaying,
-    songUrl,
-    title,
+  const song = await response.json() as SpotifyNowPlayingResponse;
+
+  // Get the biggest image from the `images` array.
+  const album_image = song.item.album.images.reduce(
+    (a, b) => Math.max(a.height, a.width) > Math.max(b.height, b.width) ? a : b
+  );
+
+  // Merge every artists of the song into a single string.
+  const artists = song.item.artists.map((_artist) => _artist.name).join(", ");
+
+  return json<ApiSpotifyCurrentlyPlayingResponse>({
+    success: true,
+    data: {
+      url: song.item.external_urls.spotify,
+      album_image,
+      
+      is_playing: song.is_playing,
+      
+      title: song.item.name,
+      artists,
+  
+      song_duration_ms: song.item.duration_ms,
+      song_progress_ms: song.progress_ms
+    }
   });
 }
